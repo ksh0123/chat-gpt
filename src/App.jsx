4de5202d8a467454414fc7dddb8be5f04836d9b8
ksh0123 <shnae.k@gmail.com>
2023-11-15 10:08:34 +0900

    Layout blocking

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 const App = () => {
-  return <div className="bg-red-100">Hello, React!</div>;
+  return (
+    <div className="bg-red-100 min-h-screen max-w-screen-md mx-auto flex">
+      <div className="bg-blue-100 w-52">Side</div>
+      <div className="bg-green-100 grow flex flex-col">
+        <div className="bg-yellow-100 grow">Main</div>
+        <div className="bg-purple-100">Chat Bar</div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
